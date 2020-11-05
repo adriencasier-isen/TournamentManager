af4c952fb0349fcd2b771e05f7fefddedee4c19b
Adrien Casier <adrien.casier@student.yncrea.fr>
2020-11-05 11:38:12 +0100

    Fixing wrong usage of static list

--- src/fr/isen/m1/tourament/competition/League.java
+++ src/fr/isen/m1/tourament/competition/League.java
@@ -5,10 +5,6 @@ import fr.isen.m1.tourament.enums.EStageType;
 public class League extends Competition {
     public League(String name, int confrontationCount, EStageType stageType, String edition, String location) {
         super(name, confrontationCount, stageType, edition, location);
-        list.add(this);
-    }
-
-    public void Finalize() {
-        list.remove(this);
+        League.list.add(this);
     }
 }
