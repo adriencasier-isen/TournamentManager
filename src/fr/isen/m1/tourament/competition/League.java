259b1fbe4cc3b804c765acd37a1589543192c5a2
Adrien Casier <adrien.casier@student.yncrea.fr>
2020-11-05 11:39:03 +0100

    League stage type hard coded

--- src/fr/isen/m1/tourament/competition/League.java
+++ src/fr/isen/m1/tourament/competition/League.java
@@ -3,8 +3,8 @@ package fr.isen.m1.tourament.competition;
 import fr.isen.m1.tourament.enums.EStageType;
 
 public class League extends Competition {
-    public League(String name, int confrontationCount, EStageType stageType, String edition, String location) {
-        super(name, confrontationCount, stageType, edition, location);
+    public League(String name, int confrontationCount, String edition, String location) {
+        super(name, confrontationCount, EStageType.LEAGUE, edition, location);
         League.list.add(this);
     }
 }
